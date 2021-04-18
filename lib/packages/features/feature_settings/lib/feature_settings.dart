fe0daf53d37fd55087c7f9553b5e1dffa43e90c6
Byron Antak <byron.antak@entelect.co.za>
2021-04-18 13:37:45 +0200

    Remove './'

--- lib/packages/features/feature_settings/lib/feature_settings.dart
+++ lib/packages/features/feature_settings/lib/feature_settings.dart
@@ -1,4 +1,4 @@
 library feature_settings;
 
-export './src/widgets/settings.dart';
-export './src/widgets/settings_screen_provider.dart';
+export 'src/widgets/settings.dart';
+export 'src/widgets/settings_screen_provider.dart';
